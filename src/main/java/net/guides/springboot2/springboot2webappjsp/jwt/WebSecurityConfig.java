ed8686231d6c8baadf105cd0895a2dd717ac19e9
katerinaalexandra <kdan9405@uni.sydney.edu.au>
2021-10-19 01:15:18 +1100

    spotfycallbakc works

--- src/main/java/net/guides/springboot2/springboot2webappjsp/jwt/WebSecurityConfig.java
+++ src/main/java/net/guides/springboot2/springboot2webappjsp/jwt/WebSecurityConfig.java
@@ -53,12 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-//                .antMatchers("/spotify/**").permitAll()
-//                .antMatchers("/spotify/callback").permitAll()
-//                .antMatchers("/spotify/get-token").permitAll()
-//                .antMatchers("/post/myposts").permitAll()
-//                .antMatchers("/post/addpost").permitAll()
-//                .antMatchers("/post/posts").permitAll()
+                .antMatchers("/spotify/callback/**").permitAll()
+                .antMatchers("/spotify/callback/").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
