ce3b899017338f3b3322e72fc8987ae7365a2acc
Jessica Yao <jyao7254@uni.sydney.edu.au>
2021-10-18 22:43:25 +1100

    centered display card

--- app/src/components/DisplayUserComponent.js
+++ app/src/components/DisplayUserComponent.js
@@ -5,6 +5,7 @@ import {useHistory, useLocation} from "react-router-dom";
 import axios from "axios";
 import UserServiceTesting from "../services/UserServiceTesting";
 import authHeader from "../services/authHeader";
+import Moria from "../DoorsOfMoria.png";
 
 //When the user clicks on the hyperlink/title of Post in Feed
 const DisplayUserComponent = () => {
@@ -16,23 +17,21 @@ const DisplayUserComponent = () => {
 
     useEffect(() => {
         const url = window.location.pathname; //get the path(minus domain name)
-        console.log(url);
+        //console.log(url);
         const username = url.split("/").pop();
-        console.log("USERNAME: " + username); // "ronald"
+        console.log("USERNAME: " + username); // e.g., "ronald"
         setUserName(username); //set the state variable
 
+        //Option 1: Fetch user details via Service class
         /*
-        //Fetch user details and store
-        UserServiceTesting.getUserByUserName(username).then((response)=>{
+        UserServiceTesting.getUserByUserName(username).then((response) => {
                 setUserDetails(response.data); //set User object into state variable
-                console.log("checking for stored data: " + allUserDetails.bio);
-                console.log("checking for stored data: " + allUserDetails.email);
-                //console.log("checking the state variable has beeen state: " + moreMovieInfo.Plot) this will cause issue
+                console.log("checking for stored data: " + response.data.email);
             })
 
          */
 
-
+        //Option 2: Using axios
         axios.get(`http://localhost:8080/update/user/${username}`, {headers: authHeader()}).then((response) => {
             console.log("checking line 43.");
             setUserDetails(response.data); //set State variable
@@ -47,70 +46,68 @@ const DisplayUserComponent = () => {
             <Header/>
             <Container fluid className={"content-body"}>
                 <Row className={"justify-content-center"}>
-                    <Col sm={6}>
+                    <Col>
+                        <Row className={"justify-content-end"}>
+                            <h2 className={"primary-text"}> User Profile</h2>
+                        </Row>
+                    </Col>
+
+                    <Col xs={3}>
                         <Container className={"rounded-card"}>
                             <Col>
-                                <h2 className={"primary-text"}> User Profile </h2>
-                                {/*  empty line below  */}
-                                <div className="col-md-3 col-sm-3 col-xs-3">&nbsp;</div>
-
-                                <Row className={"justify-content-center"}>
-
+                                <Row class={"justify-content-space-between"}>
                                     <Col>
-                                        <Row class={"justify-content-space-between"}>
-                                            <Col>
-                                                <strong>User name</strong>
-                                            </Col>
-                                            <Col>
-                                                {username}
-                                            </Col>
-                                        </Row>
-
-                                        <Row class={"justify-content-space-between"}>
-                                            <Col>
-                                                <strong>First name</strong>
-                                            </Col>
-                                            <Col>
-                                                {allUserDetails === null ? '' :  allUserDetails.firstName}
-                                            </Col>
-                                        </Row>
-
-                                        <Row class={"justify-content-space-between"}>
-                                            <Col>
-                                                <strong>Last name</strong>
-                                            </Col>
-                                            <Col>
-                                                {allUserDetails === null ? '' :  allUserDetails.lastName}
-                                            </Col>
-                                        </Row>
-
-                                        <Row class={"justify-content-space-between"}>
-                                            <Col>
-                                                <strong>Email</strong>
-                                            </Col>
-                                            <Col>
-                                                {allUserDetails === null ? '' :  allUserDetails.email}
-                                            </Col>
-                                        </Row>
-
-                                        <p> </p>
-
-                                        <p><strong> Bio </strong></p>
-                                        <p>
-                                            {allUserDetails === null ? '' :  allUserDetails.bio}
-                                        </p>
-
+                                        <strong>User name</strong>
+                                    </Col>
+                                    <Col>
+                                        {username}
                                     </Col>
                                 </Row>
+
+                                <Row class={"justify-content-space-between"}>
+                                    <Col>
+                                        <strong>First name</strong>
+                                    </Col>
+                                    <Col>
+                                        {allUserDetails === null ? '' :  allUserDetails.firstName}
+                                    </Col>
+                                </Row>
+
+                                <Row class={"justify-content-space-between"}>
+                                    <Col>
+                                        <strong>Last name</strong>
+                                    </Col>
+                                    <Col>
+                                        {allUserDetails === null ? '' :  allUserDetails.lastName}
+                                    </Col>
+                                </Row>
+
+                                <Row class={"justify-content-space-between"}>
+                                    <Col>
+                                        <strong>Email</strong>
+                                    </Col>
+                                    <Col>
+                                        {allUserDetails === null ? '' :  allUserDetails.email}
+                                    </Col>
+                                </Row>
+
+                                <p> </p>
+
+                                <p><strong> Bio </strong></p>
+                                <p>
+                                    {allUserDetails === null ? '' :  allUserDetails.bio}
+                                </p>
+
                             </Col>
 
                         </Container>
                     </Col>
-                    <Col></Col>
+                    <Col/>
                 </Row>
             </Container>
+
         </div>
-    )
+    );
 }
 
 export default DisplayUserComponent;
