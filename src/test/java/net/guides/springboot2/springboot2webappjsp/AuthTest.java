6f9c561a1dabc6cd48468b5f2685f2ac4434e2f5
nnae9426@uni.sydney.edu.au <nnae9426@uni.sydney.edu.au>
2021-10-31 06:30:53 +1100

    some tests

---
+++ src/test/java/net/guides/springboot2/springboot2webappjsp/AuthTest.java
@@ -0,0 +1,102 @@
+package net.guides.springboot2.springboot2webappjsp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.guides.springboot2.springboot2webappjsp.controllers.UserControllerTesting;
+import net.guides.springboot2.springboot2webappjsp.domain.User;
+import net.guides.springboot2.springboot2webappjsp.repositories.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.*;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(secure = false) //secure=false, solved the 401 issue
+@RunWith(SpringRunner.class) //solved issue with NullPointerException
+@WebMvcTest(UserControllerTesting.class)
+public class AuthTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private UserRepository repository; //No mocking here
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    UserRepository repo; //mocked
+
+    @MockBean
+    PasswordEncoder encoder;
+
+
+
+    User user1 = new User("HarveySpecter", "theBestCloser@gmail.com", "hs123456");
+    User user2 = new User("DonnaPaulsen", "donnapaulsen@gmail.com", "dp123456");
+    User user3 = new User("LouisLitt", "norma@gmail.com", "catGuy123456");
+
+    @Test
+    public void loginError() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        user1.setPassword("123456");
+        Map<String, String> values = new HashMap<>();
+        values.put("username", "username");
+        values.put("password", "password");
+        List<User> records = new ArrayList<>(Arrays.asList(user1, user2, user3));
+
+        Mockito.when(repo.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/login")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+    }
+    @Test
+    public void testExistByUsername() {
+
+        User user1 = new User("admin1", "admin1@gmail.com", "firstname1", "lastname1", "my bio1");
+        User user2 = new User("admin2", "admin2@gmail.com", "firstname2", "lastname2", "my bio2");
+
+        when(repository.existsByUsername("admin1")).thenReturn(true);
+        when(repository.existsByUsername("admin2")).thenReturn(true);
+
+        assertFalse(repository.existsByUsername("admin111"));
+        assertFalse(repository.existsByUsername("admin222"));
+
+        assertTrue(repository.existsByUsername("admin1"));
+        assertTrue(repository.existsByUsername("admin2"));
+
+
+
+
+    }
+}
